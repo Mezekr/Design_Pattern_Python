cf0254d7352c07d5238342415fe7556ef115ace6
Mezekr Weldu <mezekr.edu@gmail.com>
2023-07-07 22:03:23 +0200

    add singleton class

--- singleton.py
+++ singleton.py
@@ -8,3 +8,17 @@ class Borg:
         # Make the class attribute dictionary to instant attribute
         self.__dict__ = self._shared_data
 
+class Singleton(Borg):
+    """ Class to implement singleton design pattern."""
+
+    def __init__(self, **kwargs):
+        Borg.__init__(self)
+
+        # update the attributes dictionary by inserting key-value pairs
+        Borg._shared_data.update(**kwargs)
+
+        # Returns the string representation of of the object
+        def __str__(self):
+            return str(self._shared_data)
+
+
