9b03f7cfd42c32c828edd07b3eac2d87e8b2ac1e
Mezekr Weldu <mezekr.edu@gmail.com>
2023-07-07 22:15:22 +0200

    run and test singleton design pattern

--- singleton.py
+++ singleton.py
@@ -15,10 +15,19 @@ class Singleton(Borg):
         Borg.__init__(self)
 
         # update the attributes dictionary by inserting key-value pairs
-        Borg._shared_data.update(**kwargs)
+        Borg._shared_data.update(kwargs)
 
         # Returns the string representation of of the object
-        def __str__(self):
-            return str(self._shared_data)
+    def __str__(self):
+        return str(self._shared_data)
 
+# Let's create the first singleton instance and add first acronym
+HTTP_singleton_instance = Singleton(HTTP = "Hyper Text Transfer Protocol")
+print(HTTP_singleton_instance)
 
+#Let's create the second object and add second acronym
+# see if refers to the the same attribute dictionary
+
+SNMP_singleton_instance = Singleton(SNMP = "Simple Network Management Protocol")
+
+print(SNMP_singleton_instance)
