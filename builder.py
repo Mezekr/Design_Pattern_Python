538773e9485405ab9f14d72968ce124051108012
Mezekr Weldu <mezekr.edu@gmail.com>
2023-07-13 09:16:58 +0200

    add director to crete the the object

--- builder.py
+++ builder.py
@@ -31,3 +31,16 @@ class MazdaBuilder(Builder):
     
     def add_engine(self):
         self.car.engine = "Turbo Engine"
+
+class Director:
+    """ Director class to create objects """
+    def __init__(self, builder):
+        self._builder = builder
+    def constrct_car(self):
+        self._builder.create_new_car()
+        self._builder.add_name()
+        self._builder.add_model()
+        self._builder.add_engine()
+
+    def get_car(self):
+        return self._builder.car
