fc3e1dbef3aedd82b8c6b3237a15538d7e2e928b
Mezekr Weldu <mezekr.edu@gmail.com>
2023-07-18 19:01:49 +0200

    define the Proxy object

--- proxy.py
+++ proxy.py
@@ -10,3 +10,25 @@ class Producer:
 
     def meet(self):
         print("Producer has time for meeting!")
+
+class Proxy:
+    """Defines the ' Relatively less resource intensive' proxy to instantiate"""
+    
+    def __init__(self):
+        self.occupied = 'No'
+        self.producer = None
+
+    def produce(self):
+        """Check if the Producer is available"""
+        time_to_wait = 2
+        if self.occupied == 'No':
+            # If the producer is available, create a producer object
+            self.producer = Producer()
+            time.sleep(time_to_wait)
+
+            # Make the producer meet the guest
+            self.producer.meet()
+        else:
+            # Otherwise, do not instaniate a producer
+            time.sleep(time_to_wait)
+            print("Producer is busy!")
