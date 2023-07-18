a4307cb69f6fa0e4a6d208e307a364f783cb0ca1
Mezekr Weldu <mezekr.edu@gmail.com>
2023-07-18 10:00:53 +0200

    define decorator

---
+++ decorator.py
@@ -0,0 +1,19 @@
+# Decorator desgine pattern example 
+from functools import wraps
+
+def make_blink(function):
+    """Defines a decorator"""
+
+    # This makes the decorated transparent in terms of its name and docs
+
+
+    # Defines the inner function
+
+    def decorator():
+        # Grab the return value of the function being decorated
+
+
+        # add new functionality to the function being decorated
+
+        return decorator
+        
