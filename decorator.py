a9ed9f561b7c6a380f344a64384a805ac0569545
Mezekr Weldu <mezekr.edu@gmail.com>
2023-07-18 10:10:08 +0200

    apply the decorator

--- decorator.py
+++ decorator.py
@@ -5,20 +5,21 @@ def make_blink(function):
     """Defines a decorator"""
 
     # This makes the decorated transparent in terms of its name and docs
-
+    @wraps(function)
 
     # Defines the inner function
-
     def decorator():
         # Grab the return value of the function being decorated
-
+        fun_return = function()
 
         # add new functionality to the function being decorated
+        return "<blink>" + fun_return + "</blink>"
 
-        return decorator
+    return decorator
     
 
-# Defines the inner function
+# Defines the inner function and apply the decorated function
+@make_blink
 def hello_world():
     """ Original function"""
     return "Helo, World!"
