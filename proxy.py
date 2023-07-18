3ff24a5cc2b1ec00b261a10411f491cb259e3078
Mezekr Weldu <mezekr.edu@gmail.com>
2023-07-18 19:15:16 +0200

    run and test the Proxy design pattern example

--- proxy.py
+++ proxy.py
@@ -9,7 +9,7 @@ class Producer:
         print("Producer is working hard!")
 
     def meet(self):
-        print("Producer has time for meeting!")
+        print("Producer has time to meet you now!")
 
 class Proxy:
     """Defines the ' Relatively less resource intensive' proxy to instantiate"""
@@ -20,6 +20,8 @@ class Proxy:
 
     def produce(self):
         """Check if the Producer is available"""
+        print("Artist checking if the Producer is available... ")
+
         time_to_wait = 2
         if self.occupied == 'No':
             # If the producer is available, create a producer object
@@ -32,3 +34,15 @@ class Proxy:
             # Otherwise, do not instaniate a producer
             time.sleep(time_to_wait)
             print("Producer is busy!")
+
+# Instantiatre a Proxy
+proxy = Proxy()
+
+# Make the Proxy: Artist produce as long as Producer is available
+proxy.produce()
+
+# change the state to 'occupied'
+proxy.occupied = 'Yes'
+
+# Make the Producer produce
+proxy.produce()
