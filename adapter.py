9029acb5720b9ebbdfd12a05c7f259ca01c595d0
Mezekr Weldu <mezekr.edu@gmail.com>
2023-07-19 08:40:42 +0200

    define the adapter class

--- adapter.py
+++ adapter.py
@@ -16,3 +16,19 @@ class British:
 
     def speak_English(self):
         return "Hello, World!"
+
+class Adapter:
+    """Changes the generic method name to individual method name"""
+
+    def __init__(self, object, **adapter_method):
+        """Changes the name of the method"""
+        self._object = object
+
+        # Add a new dictionary item that establishes the mapping between the methods
+        # For example, speak() will be translated to speak_tigrinya() 
+        self.__dict__.update(adapter_method)
+
+    def __getattr__(self, attr):
+        """Returns the rest of the attributes!"""
+        return getattr(self._object, attr)
+    
