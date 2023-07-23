740321d07691281a1da953084b645ea7775d447b
Mezekr Weldu <mezekr.edu@gmail.com>
2023-07-23 09:26:59 +0200

    defien concreate child class

--- composite.py
+++ composite.py
@@ -1,10 +1,24 @@
 # Composite desgin pattern example
 
-class Composite:
+class Component:
     """Abstract class"""
 
     def __init__(self,*args, **kwargs):
         pass
-
-    def composite_functions(self):
+    def component_functions(self):
         pass
+
+# Inherits from Composite anbstract class
+class Child(Component):
+    """Concrete class"""
+
+    def __init__(self,*args, **kwargs):
+        Component.__init__(self, *args, **kwargs)
+
+    # This is where the the Name of the Child item stores!
+    self.name = args[0]
+
+    # define the abstract method component_function of the parent abstract  class
+    def component_function(self):
+        # prints Name of the Child item
+        print(f"{self.name}")
