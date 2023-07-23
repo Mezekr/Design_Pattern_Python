78470abd8e56d6cf64625f84e285282916d34c82
Mezekr Weldu <mezekr.edu@gmail.com>
2023-07-23 09:15:30 +0200

    define composite abstract class

---
+++ composite.py
@@ -0,0 +1,10 @@
+# Composite desgin pattern example
+
+class Composite:
+    """Abstract class"""
+
+    def __init__(self,*args, **kwargs):
+        pass
+
+    def composite_functions(self):
+        pass
