6d119b7a97016fe49c6cf7615fe8170e214b5b5b
Mezekr Weldu <mezekr.edu@gmail.com>
2023-07-23 09:40:47 +0200

    defien concreate Composite class

--- composite.py
+++ composite.py
@@ -22,3 +22,31 @@ class Child(Component):
     def component_function(self):
         # prints Name of the Child item
         print(f"{self.name}")
+
+# Create a class that inherits from the abstract class, Component
+class Composite(Component):
+    """Concrete class and maintains the tree recursive structure"""
+
+    def __init__(self, *args, **kwargs):
+        Component.__init__(self, *args, **kwargs)
+
+    # This is where the the Name of the composite object stores
+    self.name = args[0]
+
+    # List, where the the Child item keeps 
+    self.children = []
+
+    def child_append(self, child):
+        """ Method to add a new child item"""
+        self.child_append(child)
+    def child_remove(self, child):
+        """ Method to remove a child"""
+        self.children.remove(child)
+
+    def component_functions(self):
+        # Print the name of the composite object
+        print(f"{self.name}")
+
+        # Iterate through the child objects and invoke the component functions
+        for child in self.children:
+            child.component_function()
